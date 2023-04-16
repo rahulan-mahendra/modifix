7f73164f085942920792deef1a68d19cb1a15822
rahulan-mahendra <rahulmahendra24@gmail.com>
2023-04-16 21:33:51 +0530

    User.jsx_Changes

--- admin-app/src/views/users/Users.jsx
+++ admin-app/src/views/users/Users.jsx
@@ -36,7 +36,7 @@ const Users = () => {
 
             <div className="card shadow mb-4">
                 <div className="card-header py-3">
-                    <h6 className="m-0 font-weight-bold text-primary">Users </h6>
+                    <h6 className="m-0 font-weight-bold text-primary">Users Table</h6>
                 </div>
                 <div className="card-body">
                     <div className="table-responsive">
